b2d9f910a09a14636e22f359b3281eefee890b7b
abdusalom <boriboyevabdusalom@gmail.com>
2023-12-11 10:35:08 +0500

    7

--- file07.py
+++ file07.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    
+    s = 0
+    for i in data:
+        if i.isdigit():
+            s+=int(i)
+    return s
 # Read data from file
+f = open("data/data07.txt")
+s = f.read()
+print(main(s))
