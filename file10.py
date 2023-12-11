3b668a96f73c21a61544f89c3acfbb4c18a8ba99
abdusalom <boriboyevabdusalom@gmail.com>
2023-12-11 10:39:14 +0500

    10

--- file10.py
+++ file10.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    data = data.split("\n")
+    mx = len(data[0])
 
+    for i in data:
+        if mx <len(i):
+            mx = len(i)
+    return mx
 # Read data from file
+f = open("data/data10.txt")
+s = f.read()
+print(main(s))
